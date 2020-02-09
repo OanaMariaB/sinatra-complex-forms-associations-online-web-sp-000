94a8a8fe5bc3b6d960d8262b3370f8060f59470d
Oana Berentan <oana.berentan@yahoo.com>
2020-02-09 21:32:17 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -28,6 +28,11 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
+  get '/pets/:id/edit' do
+   @pet = Pet.find_by_id(params[:id])
+   erb :'/pets/edit'
+ end
+
   patch '/pets/:id' do
     @pet = Pet.find_by_id(params[:id])
     @pet.update(params["pet"])
